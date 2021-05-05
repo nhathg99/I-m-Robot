3550bc5262ae1f7df5df4df3f62d92e117d4c6a5
nhathg99 <81789723+nhathg99@users.noreply.github.com>
2021-05-05 09:31:58 +0700

    Update Practice 2.c

--- Practice C/Test/Practice 2.c
+++ Practice C/Test/Practice 2.c
@@ -5,11 +5,18 @@
 int lowerCase(char s[100]){
 	
 	printf("Nhap chuoi ki tu cua ban:");
+	int i,size;
 	gets(s);
-	
-	strlwr(s);
-	printf("Chuoi ki tu lowerCase la : %s",s);
-	return 0;
+	size = strlen(s);
+	for(i=0;i<size;i++){
+		if(s[i]>=65 && s[i]<=140)
+		{
+			s[i] += 32;
+		}
+	}
+	for(i=0;i<size;i++){
+		printf("%c",s[i]);
+	}
 }
 //=============================================================
 void main() {
